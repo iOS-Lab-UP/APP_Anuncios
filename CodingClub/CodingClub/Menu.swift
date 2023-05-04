ccf224c8616842c808e37129f0f0784161adbea9
saramirandaa <0244643@up.edu.mx>
2023-05-04 10:25:21 -0600

    front

--- CodingClub/CodingClub/Menu.swift
+++ CodingClub/CodingClub/Menu.swift
@@ -9,13 +9,97 @@ import SwiftUI
 
 struct Menu: View {
     @State private var isShowingDetail = false
+    @State private var username = ""
     var body: some View {
-        Text("Menu")
-    }
-}
+        
+        ZStack
+        {
+            Color.orange
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                ZStack {
+                        Rectangle()
+                            .fill(Color.white)
+                            .frame(width: 390, height: 550)
+                            .cornerRadius(45)
+                    VStack {
+                        HStack {
+                            Text("  COMUNIDAD")
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                Spacer()
+                        }
+                        HStack {
+                            Text("  UP")
+                                .font(.largeTitle)
+                            .fontWeight(.bold)
+                            Spacer()
+                        }
+                        HStack {
+                            Text("  Email")
+                            Spacer()
+                        }
+                            
+                        VStack {
+                            ZStack {
+                                Rectangle()
+                                    .fill(Color.gray)
+                                    .frame(width: 300, height: 50)
+                                    .cornerRadius(45)
+                                HStack {
+                                    TextField(
+                                            "User name (email address)",
+                                            text: $username
+                                    ).padding(20)
+                                }
+                            }
+                            Text("Selecciona")
+                        
+                                HStack
+                                {
+                                    ZStack {
+                                        Rectangle()
+                                            .fill(Color.gray)
+                                            .frame(width: 100, height: 50)
+                                            .cornerRadius(45)
+                                        TextField(
+                                                "Student",
+                                                text: $username
+                                        ).padding(20)
+                                    }
+                                    ZStack {
+                                        Rectangle()
+                                            .fill(Color.gray)
+                                            .frame(width: 100, height: 50)
+                                            .cornerRadius(45)
+                                        TextField(
+                                                "Docente",
+                                                text: $username
+                                        ).padding(20)
+                                    }
+                                }
+                            Rectangle()
+                                .fill(Color.white)
+                                .frame(width: 100, height: 150)
+                                .cornerRadius(45)
+                            
+                            Text("Next")
+                        }
+                        
+                        
+                    }
 
-struct Menu_Previews: PreviewProvider {
-    static var previews: some View {
-        Menu()
+                }
+                
+            }.padding(.top, 20)
+                .ignoresSafeArea()
+        }
+    }
+    
+    struct Menu_Previews: PreviewProvider {
+        static var previews: some View {
+            Menu()
+        }
     }
 }
---
+++ CodingClub/CodingClub/View2.swift
@@ -0,0 +1,20 @@
+//
+//  View2.swift
+//  CodingClub
+//
+//  Created by Sara Miranda on 03/05/23.
+//
+
+import SwiftUI
+
+struct View2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct View2_Previews: PreviewProvider {
+    static var previews: some View {
+        View2()
+    }
+}
